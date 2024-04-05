707d921d4ebd6a0683b61a42c8aa97f9d58ec788
nablet <nablet.chew@gmail.com>
2024-04-05 18:52:27 +0800

    Update problem003

--- Problem003.py
+++ Problem003.py
@@ -3,17 +3,26 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-#num = 13195
-num = 600851475143
-primes = []
-i = 2
+from timer import timed
 
-while num > 2:
-	if num % i == 0:
-		primes.append(i)
-		num /= i
-		i = 2
-	else:
-		i += 1
 
-print(primes)
+@timed
+def problem_003():
+    #num = 13195
+    num = 600851475143
+    primes = []
+    i = 2
+
+    while num > 2:
+        if num % i == 0:
+            primes.append(i)
+            num /= i
+            i = 2
+        else:
+            i += 1
+
+    return max(primes)
+
+
+if __name__ == '__main__':
+    print(f"answer: {problem_003()}")
