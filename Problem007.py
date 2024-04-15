8177f38fa572c30775a437ac5345e6c09c33d234
JaysonTi <jaysonti@cloudstaff.com>
2024-04-15 20:45:09 +0800

    Update problem007

--- Problem007.py
+++ Problem007.py
@@ -3,24 +3,25 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
-import math, time
+from timer import timed
+import math
 
-s = time.time()
 
-num = 1
-counter = 0
+@timed
+def problem_007() -> int:
+	counter = 0
+	num = 1
 
-while True:
-	num += 1
-	for i in range(2, int(math.sqrt(num)) + 1):
-		if (num % i) == 0:
-			break
-	else:
-		counter += 1
-		#print(str(num) + ' ' + str(counter))
-		if counter == 10001:
-			print(str(num) + ' ' + str(counter))
-			print(time.time() - s)
-			break
+	while True:
+		num += 1
+		for i in range(2, int(math.sqrt(num)) + 1):
+			if (num % i) == 0:
+				break
+		else:
+			counter += 1
+			if counter == 1001:
+				return num
 
-# IMPROVED PROCESS EFFICIENCY
+
+if __name__ == '__main__':
+	print(f"answer: {problem_007()}")
